46881816b4344f86b9a82a76b663dd531659fcdd
eduardoperezstudent <pereze4@oregonstate.edu>
2025-03-13 02:53:11 -0700

    Initial

---
+++ Proj6_pereze4.asm
@@ -0,0 +1,31 @@
+TITLE Temp List Reverser     (Proj6_pereze4.asm)
+
+; Author: Eduardo Perez
+; Last Modified: March 3, 2025
+; OSU email address: pereze4@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number: 6               Due Date: March 3, 2025
+; Description: This file is provided as a template from which you may work
+;              when developing assembly projects in CS271.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
